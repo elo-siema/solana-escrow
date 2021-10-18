2f5e73d9c16b51a6b74b0dd3aff203ca7f0f1844
Michał Flak <mflak@monadical.com>
2021-10-19 01:34:19 +0200

    Finished, including bugfix

---
+++ src/error.rs
@@ -0,0 +1,22 @@
+use solana_program::program_error::ProgramError;
+// inside error.rs
+use thiserror::Error;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    #[error("Not rent exempt")]
+    NotRentExempt,
+    #[error("Expected Amount Mismatch")]
+    ExpectedAmountMismatch,
+    #[error("Amount Overflow")]
+    AmountOverflow,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
